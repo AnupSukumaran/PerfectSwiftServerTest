4d298b808eb54beab027a89c1adf0ae5658467fe
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-08-20 21:33:20 +0530

    - first commit

--- PerfectSwiftServerTest/ViewController.swift
+++ PerfectSwiftServerTest/ViewController.swift
@@ -9,12 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var operandTwoField: UITextField!
+    @IBOutlet weak var operandOneField: UITextField!
+    @IBOutlet weak var sumField: UITextField!
+    
+    private let endPoint = "http://localhost:8181/add"
+    private let urlSession = URLSession.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func add(_ sender: UIButton) {
+        
+        guard let numberOneText = operandOneField.text, let numberTwoText = operandTwoField.text else {return}
+        
+        if let numberOne = Int(numberOneText), let numberTwo = Int(numberTwoText) {
+            addNumber(numberOne: numberOne, numberTwo: numberTwo)
+        }
+        
+    }
+    
+    private func addNumber(numberOne: Int, numberTwo: Int) {
+        let parameters = ["operandOne": numberOne, "operandTwo": numberTwo]
+        
+        guard let urlToExecute = URL(string: endPoint) else {return}
+        
+        var webRequest = URLRequest(url: urlToExecute)
+        webRequest.httpMethod = "POST"
+        webRequest.addValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        let urlParams = parameters.compactMap { (key, value) in
+            
+            "\(key)=\(value)"
+        }.joined(separator: "&")
+        
+        webRequest.httpBody = urlParams.data(using: .utf8, allowLossyConversion: true)
+        
+        let dataTask = urlSession.dataTask(with: webRequest) { (data, response, error) in
+            print("response received from server")
+            guard let data = data, let res = response, error == nil else {
+                print("error = \(error!.localizedDescription)")
+                return
+                
+            }
+            print("res = \(res)")
+            
+            do {
+                let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                print("jsonResponse = \(jsonResponse)")
+                
+                guard let sum = jsonResponse?["sum"] as? Int else {return}
+                
+                DispatchQueue.main.async { [unowned self] in
+                    self.sumField.text = String(sum)
+                }
+                       
+            } catch let error {
+                print("Error = \(error.localizedDescription)")
+            }
+           
+        }
+        dataTask.resume()
+    }
+    
 
 }
 
